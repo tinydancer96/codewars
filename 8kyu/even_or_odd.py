ca89738938092406b825b631b7632ffbab902d8a
Mardgelle Neri <mardgelleneri@gmail.com>
2023-11-08 08:02:31 +0000

    new challenge added

---
+++ 8kyu/even_or_odd.py
@@ -0,0 +1,7 @@
+# DESCRIPTION:
+# Create a function that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
+
+
+# My solution
+def even_or_odd(number):
+    return "Even" if number % 2 == 0 else "Odd"

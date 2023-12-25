68e3c9ec4555f2bf46f9cd925b90c5c26985d3f8
Mardgelle Neri <mardgelleneri@gmail.com>
2023-12-25 15:05:40 +0000

    completed dinner

---
+++ 7Kyu/dinner.py
@@ -0,0 +1,17 @@
+# The discussion gets heated and you are cannot risk favoring either of them as this might damage your political standing with either of the two clans the samurai generals belong to. Thus, the only thing left to do is find what the common ground of what they are saying is.
+
+# Compare the proposals with the following function:
+
+# def common_ground(s1,s2)
+# The parameters s1 and s2 are the strings representing what each of the generals said. You should output a string containing the words in s1 that also occur in s2.
+
+# Each word in the resulting string shall occur once, and the order of the words need to follow the order of the first occurence of each word in s2.
+
+# If they are saying nothing in common, kill both samurai and blame a ninja. (output "death")
+
+def common_ground(s1,s2):
+    lst = []
+    for w in s2.split():
+        if w in s1.split() and w not in lst:
+            lst.append(w)
+    return ' '.join(lst) if lst else "death"
